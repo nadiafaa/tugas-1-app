5209acde0b47ba2469c199c9c6b9412c27f58df2
Nadia Fadillah <nadiafadillah055@gmail.com>
2025-07-17 21:49:45 +0700

    Update tugas 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Navbar from "@/components/Navbar"; // Navbar ini Client
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Apotek Sehat",
