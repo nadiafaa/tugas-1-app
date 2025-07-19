1fe81ffaf297c5a197196e8d0a3111c48e70d557
Nadia Fa <nadiafadilah527@email.com>
2025-07-19 19:06:11 +0700

    fixed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,28 +5,9 @@ export default async function ProductPage({
 }) {
   const { id } = params;
 
-  const product = {
-    id,
-    name: `Produk Apotek ${id}`,
-    description: "Ini detail produk apotek.",
-    isReady: true,
-  };
-
   return (
     <main className="p-8">
-      <h1 className="text-3xl font-bold mb-4 text-blue-600">
-        Detail Produk #{product.id}
-      </h1>
-      <p className="mb-2 text-lg">
-        Nama Produk: <strong>{product.name}</strong>
-      </p>
-      <p className="mb-2">Deskripsi: {product.description}</p>
-      <p className="mb-2">
-        Status:{" "}
-        <span className={product.isReady ? "text-green-600" : "text-red-600"}>
-          {product.isReady ? "Ready di Apotek" : "Tidak Tersedia"}
-        </span>
-      </p>
+      <h1>{id}</h1>
     </main>
   );
 }
